a0ffcbe9a4e03e13c1b72e5aec077f2094fe3738
GenaPlem <shainkill1337@gmail.com>
2024-10-18 16:20:40 +0100

    Add Link to Meals page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
+      <h1 style={{ color: "white", textAlign: "center" }}>
         Time to get started!
       </h1>
+      <nav>
+        <Link href="/meals">Meals Here</Link>
+      </nav>
     </main>
   );
 }
