c7dc90a6ea9b3a67650b2089789a630e102c1760
GenaPlem <shainkill1337@gmail.com>
2024-10-22 16:20:13 +0100

    Add not-found page for meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,14 @@
+import { notFound } from "next/navigation";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { getMeal } from "@/lib/meals";
 
 const MealPage = ({ params }) => {
   const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   meal.instructions = meal.instructions.replace(/\n/g, "<br />");
 
