ee6143932d740e92f110112eeceaf4fa06aff3a7
GenaPlem <shainkill1337@gmail.com>
2024-10-22 15:24:08 +0100

    Add Error component for meals

---
+++ app/meals/error.js
@@ -0,0 +1,13 @@
+"use client";
+
+const Error = ({ error }) => {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to fetch meal data. Please try again later.</p>
+      <p>{error.message}</p>
+    </main>
+  );
+};
+
+export default Error;
