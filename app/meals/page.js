e4aa68aefb1ad0e20bf326c4ed1db9fdb57001dc
GenaPlem <shainkill1337@gmail.com>
2024-10-22 15:15:58 +0100

    Add loading with Suspense while loading data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import { Suspense } from "react";
+
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={styles.header}>
@@ -21,7 +27,11 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={styles.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={styles.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
