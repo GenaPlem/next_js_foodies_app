6bc160b55c957837343aaed4f735ecd48e3f1d4c
GenaPlem <shainkill1337@gmail.com>
2024-10-23 10:58:32 +0100

    Add server action to get data from form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,21 @@ import ImagePicker from "@/components/meals/image-picker";
 import styles from "./page.module.css";
 
 const ShareMealPage = () => {
+  const shareMeal = async (formData) => {
+    "use server";
+
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  };
+
   return (
     <>
       <header className={styles.header}>
@@ -11,7 +26,7 @@ const ShareMealPage = () => {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={shareMeal}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +54,7 @@ const ShareMealPage = () => {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
             <button type="submit">Share Meal</button>
           </p>
