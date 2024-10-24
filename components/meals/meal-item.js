e546de8e858ed5fd3cf7e6e454b181497de3b68e
GenaPlem <shainkill1337@gmail.com>
2024-10-24 11:21:09 +0100

    Update meal item images src

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -4,11 +4,17 @@ import Image from "next/image";
 import styles from "./meal-item.module.css";
 
 const MealItem = ({ title, slug, image, summary, creator }) => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+
   return (
     <article className={styles.meal}>
       <header>
         <div className={styles.image}>
-          <Image src={image} alt={title} fill />
+          <Image
+            src={`${supabaseUrl}/storage/v1/object/public/FoodiesApp/${image}`}
+            alt={title}
+            fill
+          />
         </div>
         <div className={styles.headerText}>
           <h2>{title}</h2>
